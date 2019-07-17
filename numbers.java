c614a27dd8e186e50d45107dcc4a20916d208fcc
Susheel1998 <32878785+Susheel1998@users.noreply.github.com>
2019-07-17 10:14:30 +0530

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class numbers {
+public static void main(String args[]) {
+Scanner in = new Scanner(System.in);
+int n = in.nextInt();
+
+if(n>0) {
+System.out.println("Positive");
+}
+else if(n<0) {
+System.out.println("Negative");
+}
+else {
+System.out.println("Zero");
+}
+}
+}
