9bbb35bf19c25f234cddf6c779233394a44a5743
Susheel1998 <32878785+Susheel1998@users.noreply.github.com>
2019-07-17 10:26:25 +0530

    Create A1.java

---
+++ A1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class numbers {
+public static void main(String args[]) {
+Scanner in = new Scanner(System.in);
+int n = in.nextInt();
+
+if(n%2 == 0) {
+System.out.println("Even");
+}
+else if(n%2 != 0) {
+System.out.println("Odd");
+}
+else {
+System.out.println("Invalid");
+}
+}
+}
